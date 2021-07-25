cf7f79e1cbef3e1993b83a54d9ee95f1dcf73bb0
Mark Kenneth M. Bayona <mkmbayona@gmail.com>
2021-07-25 17:58:37 +0800

    UISlider changed action Show uislider formatted value on to a label Fixed initial slider position

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var heightValueLabel: UILabel!
+    @IBOutlet weak var weightValueLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightValueLabel.text = String(format: "%.2fm", sender.value)
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValueLabel.text = String(format: "%.0fKg", sender.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        heightSlider.value = 1.5
+        weightSlider.value = 100
     }
-
-
+    
 }
 
